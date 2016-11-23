248394f074722616127b261709e733ba78d26c71
boundlessly <540917349@qq.com>
2016-11-23 23:20:19 +0800

    aaaa

---
+++ app/src/main/java/com/example/administrator/myapplications/SecondActivity.java
@@ -0,0 +1,31 @@
+package com.example.administrator.myapplications;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Button button = (Button) findViewById(R.id.botton_1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
